7d7723f88850ccd19ce50f54fec3cb06e488a130
Ewa Mitulska-Wójcik <thedoerdoes@gmail.com>
2016-07-19 17:56:57 +0200

    set tweet content and introduce layout changes

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var currentTime = 25;
 var breakTime = 5;
-var goal = "test";
+var goal = "something awesome";
 var workingTimeSecs = 0;
 var min;
 var intervalHandler = null;
@@ -14,31 +14,43 @@ function convertToMinutes(seconds) {
   return Math.floor(seconds / 60);
 }
 
+function timeCustomization() {
+  $(".current-timer .minus-btn").on("click", function(){
+    if (!$(this).hasClass("disabled")) {
+      if (currentTime > 0) {
+        currentTime -= 1
+      }
+      displayCurrentWorkTime();
+    }
+  });
+  $(".current-timer .add-btn").on("click", function(){
+    if (!$(this).hasClass("disabled")) {
+      currentTime += 1;
+      displayCurrentWorkTime();
+    }
+  });
+  $(".future-timer .minus-btn").on("click", function(){
+    if (!$(this).hasClass("disabled")) {
+      if (breakTime > 0) {
+      breakTime -= 1;
+      }
+      $("#break-time").text(breakTime);  
+    }
+  });
+  $(".future-timer .add-btn").on("click", function(){
+    if (!$(this).hasClass("disabled")) {
+      breakTime += 1;
+      $("#break-time").text(breakTime);
+    }
+  });
+}
+
 $(document).ready(function() {
   displayCurrentWorkTime();
   $("#break-time").text(breakTime);
   
   // time spans customization
-  $(".current-timer .minus-btn").on("click", function(){
-    if (currentTime > 0) {
-      currentTime -= 1
-    }
-    displayCurrentWorkTime();
-  });
-  $(".current-timer .add-btn").on("click", function(){
-    currentTime += 1;
-    displayCurrentWorkTime();
-  });
-  $(".future-timer .minus-btn").on("click", function(){
-    if (breakTime > 0) {
-      breakTime -= 1;
-    }
-    $("#break-time").text(breakTime);
-  });
-  $(".future-timer .add-btn").on("click", function(){
-    breakTime += 1;
-    $("#break-time").text(breakTime);
-  });
+  timeCustomization();
   
   // starting countdown of work time
   $("#start-btn").on("click", function() {
@@ -51,12 +63,19 @@ $(document).ready(function() {
     }, 1000);
     $("#start-btn").addClass("hidden");
     $("#pause-btn").removeClass("hidden");
+    $(".add-btn").addClass("disabled");
+    $(".minus-btn").addClass("disabled");
   });
   
   $("#pause-btn").on("click", function() {
     clearInterval(intervalHandler);
     $("#start-btn").removeClass("hidden");
     $("#pause-btn").addClass("hidden");
+  });
+  
+  // saving the goal
+  $("#goal").on("keyup change", function() {
+     goal = $("#goal").val();
   });
   
   // set up for tweets
